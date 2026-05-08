ff3febf29ad9f4a7e05d04c58a2e213167002653
Miguel Apilanez <miguelapilanez2@gmail.com>
2026-05-08 20:24:50 +0200

    animacion correr personaje

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Movimiento")]
-    public float speed = 5f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 9f;
     public float rotationSpeed = 10f;
 
     [Header("Salto")]
@@ -27,23 +28,39 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position,groundDistance,groundMask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = new Vector3(x, 0f, z).normalized;
 
-        animator.SetFloat("Speed", move.magnitude);
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
 
         if (move.magnitude >= 0.1f)
         {
-            Vector3 movement = move * speed * Time.deltaTime;
+            Vector3 movement = move * currentSpeed * Time.deltaTime;
+
             transform.position += movement;
 
             Quaternion targetRotation = Quaternion.LookRotation(move);
 
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        if (move.magnitude == 0)
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+        else if (isRunning)
+        {
+            animator.SetFloat("Speed", 1f);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0.5f);
         }
 
         if (Input.GetButtonDown("Jump") && isGrounded)
@@ -55,12 +72,8 @@ public class PlayerController : MonoBehaviour
             animator.SetTrigger("Jump");
         }
     }
-
     void OnDrawGizmosSelected()
     {
-        if (groundCheck == null)
-            return;
-
         Gizmos.color = Color.red;
 
         Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
